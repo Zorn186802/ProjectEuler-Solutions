89c8c2bf824540d4b0dfeaf45a2f2ab9f00b724e
Antoine Cauchois <antoinepico@msn.com>
2019-03-23 16:19:29 +0100

    [C#] Add Project012
    Complete solution for ProjectEuler problem #12.

---
+++ C#/Problem012/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Problem012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong index = 0;
+            ulong maxCpt = 0;
+            ulong cpt = 0;
+            ulong indexSqrt = 0;
+
+            for (ulong i = 1; i < ulong.MaxValue; i++)
+            {
+                cpt = 0;
+                index += i;
+                indexSqrt = (ulong)Math.Sqrt(index);
+
+                for (ulong j = 1; j <= indexSqrt; j++)
+                {
+                    if (index % j == 0)
+                    {
+                        cpt += 2;
+                    }
+                }
+                // Perfect square
+                if (indexSqrt * indexSqrt == index)
+                {
+                    cpt--;
+                }
+
+                if (cpt > maxCpt)
+                {
+                    maxCpt = cpt;
+                    if (maxCpt >= 500)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Result = " + index);
+        }
+    }
+}
