2c3e975afe8310a01e656b29a6244f02d2058e69
Antoine Cauchois <antoinepico@msn.com>
2019-03-23 14:41:07 +0100

    [C#] Add Project009
    Complete solution for ProjectEuler problem #9.

---
+++ C#/Problem009/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 1000;
+            int cpowed = 0;
+
+
+            for (int a = 1; ; a++)
+            {
+                for (int b = (a + 1); (a + b + (int)Math.Sqrt(cpowed)) <= result; b++)
+                {
+                    cpowed = (int)(Math.Pow(a, 2) + Math.Pow(b, 2));
+                    if ((Math.Pow(a, 2) + Math.Pow(b, 2)) == cpowed)
+                    {
+                        if ((a + b + Math.Sqrt(cpowed)) == result)
+                        {
+                            Console.WriteLine("a=" + a + " b=" + b + " c=" + Math.Sqrt(cpowed));
+                            Console.WriteLine("a+b+c=" + (a + b + Math.Sqrt(cpowed)));
+                            Console.WriteLine("a*b*c=" + (a * b * Math.Sqrt(cpowed)));
+                            Environment.Exit(0);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
